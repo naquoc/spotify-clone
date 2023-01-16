cf478e5ca57ba4d7152bba73432e3b87350a6471
quocna <quocnguyenanh96@gmail.com>
2023-01-17 04:13:39 +0700

    fix nested a when using NextLink and LinkOverlay

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -58,19 +58,17 @@ const Sidebar = () => {
     <Box width='100%' height='calc(100vh - 100px)' bg='black' paddingX='5px' color='gray'>
       <Box paddingY='20px' height='100%'>
         <Box width='120px' marginBottom='20px' paddingX='20px'>
-          <NextImage src="/spotify.svg" alt='logo' height={60} width={120} />
+          <NextImage src="/spotify.svg" alt='logo' height={60} width={120} priority />
         </Box>
         <Box marginBottom='20px'>
           <List spacing={2}>
             {navMenu.map(menu => (
               <ListItem paddingX='20px' fontSize='16px' key={menu.name}>
                 <LinkBox>
-                  <NextLink href={menu.route} passHref>
-                    <LinkOverlay>
-                      <ListIcon as={menu.icon} color="white" marginRight='20px' />
-                      {menu.name}
-                    </LinkOverlay>
-                  </NextLink>
+                  <LinkOverlay as={NextLink} href={menu.route}>
+                    <ListIcon as={menu.icon} color="white" marginRight='20px' />
+                    {menu.name}
+                  </LinkOverlay>
                 </LinkBox>
               </ListItem>
             ))}
@@ -82,12 +80,10 @@ const Sidebar = () => {
             {musicMenu.map(menu => (
               <ListItem paddingX='20px' fontSize='16px' key={menu.name}>
                 <LinkBox>
-                  <NextLink href={menu.route} passHref>
-                    <LinkOverlay>
-                      <ListIcon as={menu.icon} color='white' marginRight='20px' />
-                      {menu.name}
-                    </LinkOverlay>
-                  </NextLink>
+                  <LinkOverlay as={NextLink} href={menu.route}>
+                    <ListIcon as={menu.icon} color='white' marginRight='20px' />
+                    {menu.name}
+                  </LinkOverlay>
                 </LinkBox>
               </ListItem>
             ))}
@@ -99,9 +95,9 @@ const Sidebar = () => {
             {playlists.map(playlist => (
               <ListItem paddingX='20px' key={playlist}>
                 <LinkBox>
-                  <NextLink href='/'>
+                  <LinkOverlay as={NextLink} href='/'>
                     {playlist}
-                  </NextLink>
+                  </LinkOverlay>
                 </LinkBox>
               </ListItem>
             ))}
