cc5391e85aa336d785bc59284f58ad09d200d042
quocna <quocnguyenanh96@gmail.com>
2023-01-18 13:19:49 +0700

    add protected API route (verification)

---
+++ app/lib/auth.ts
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from './prisma'
+
+interface JwtPayload {
+  id: number
+}
+
+export const validateRoute = (handler: any) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.SPOTIFY_CLONE_ACCESS_TOKEN
+
+    let user
+
+    if (token) {
+      try {
+        const { id } = jwt.verify(token, 'hello') as JwtPayload
+        user = await prisma.user.findUnique({
+          where: { id }
+        })
+
+        if (!user) {
+          throw new Error('This user is no exist!')
+        }
+      } catch (error) {
+        res.status(401)
+        res.json({ error: 'Not Authorized' })
+        return
+      }
+
+      return handler(req, res, user)
+    }
+
+    res.status(401)
+    res.json({ error: 'Not Authorized' })
+  }
+
+}
---
+++ app/pages/api/me.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { validateRoute } from "../../lib/auth";
+
+export default validateRoute((req: NextApiRequest, res: NextApiResponse, user: any) => {
+  res.json(user)
+})
