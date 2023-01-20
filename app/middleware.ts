395f3ac3b91469aa378dc6e78552094510e55bdb
quocna <quocnguyenanh96@gmail.com>
2023-01-20 12:31:54 +0700

    refactor

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
-const signedinPages = ['/', '/playlist', '/library']
-
 export default function middleware(req: NextRequest) {
-  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.get('SPOTIFY_CLONE_ACCESS_TOKEN')
+  const token = req.cookies.get('SPOTIFY_CLONE_ACCESS_TOKEN')
 
-    if (!token) {
-      return NextResponse.redirect(new URL('/signin', req.url))
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url))
   }
+}
+
+export const config = {
+  matcher: ['/', '/playlist', '/playlist/(.*)', '/library']
 }
